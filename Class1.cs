0bbb5722ef590b4c2ed569377ded1122995a3968
Utente <Utente@DESKTOP-QIP6QM3>
2023-11-30 19:18:00 +0100

    Aggiungere i file di progetto.

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Quadrato
+    {
+        public int proprietà { get; set; }
+        public float sezione;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrato c= new Quadrato();
+           Quadrato c1 = c;
+            Console.WriteLine("inserisci il valore del lato "); 
+            c1.sezione=float.Parse(Console.ReadLine());
+            float superfice; 
+            Console.WriteLine("il valore della superfice del quadrato è {0}",c1.sezione*c1.sezione);
+            Console.ReadLine();
+
+        }
+    }
+}
